1d8fbb640cb74c07eb4b37136eb9d6c7fba1750b
Nanomancer <nanomancer@live.co.uk>
2015-09-10 18:20:22 +0100

    Encased parts in loops, for cue logging clarity

--- Codewave_0.1.rb
+++ Codewave_0.1.rb
@@ -6,59 +6,65 @@
 set_volume! 3
 set_sched_ahead_time! 6
 use_cue_logging true
-notes = chord(:c1, :minor, num_octaves: 2).shuffle
-#notes = scale(:c1, :hungarian_minor)
-puts notes
+#notes = chord(:c1, :minor, num_octaves: 2).shuffle
 
 live_loop :res_pulse do
+  notes = chord(:c1, :minor, num_octaves: 2).shuffle
+  4.times do
+    #cue :none
+    with_fx :reverb, mix: 0.5, room: 0.7 do
+      with_fx :lpf, cutoff: 100 do
+        with_fx :slicer, phase: [0.25, 0.5, 0.75, 1, 1.5, 2].choose do
 
-  #cue :none
-  with_fx :reverb, mix: 0.5, room: 0.7 do
-    with_fx :lpf, cutoff: 100 do
-      with_fx :slicer, phase: [0.25, 0.5, 0.75, 1, 1.5, 2].choose do
-        2.times do
-          frq = midi_to_hz(notes.tick)
-          del = 1.0 / frq
-          with_fx :echo, amp: 0.25, mix: 0.6, phase: del, decay: 1 do
-            sample :ambi_haunted_hum, pan: -0.5, amp: 0.3, rate: (ring -0.5, -1, -0.25).tick(:ambi)
+          2.times do
+
+            frq = midi_to_hz(notes.tick)
+            with_fx :echo, amp: 0.3, mix: 0.8, phase: 1.0 / frq, decay: 2 do
+              sample :ambi_haunted_hum, beat_stretch: 4, pan: -0.5, amp: 0.35, rate: (ring -0.25, -0.5, -1, -2).tick(:ambi)
+            end
+            sleep [16, 8, 4, 2].ring.look(:ambi)
+
+            frq = midi_to_hz(notes.tick)
+            with_fx :echo, amp: 0.3, mix: 0.8, phase: 1.0 / frq, decay: 2 do
+              sample :ambi_haunted_hum, beat_stretch: 4, pan: -0.5, amp: 0.35, rate: (ring 0.25, 0.5, 1, 2).look(:ambi)
+            end
+            sleep [16, 8, 4, 2].ring.look(:ambi)
+            #tick
           end
-          sleep 8
-          tick
+          sleep 4
         end
-        sleep 4
       end
     end
   end
+  #stop
 end
 
 live_loop :choir do
-
+  notes = chord(:c1, :minor, num_octaves: 2).shuffle
   #sync :none
-  tick_set 2
-  frq = midi_to_hz(notes.tick)
-  del = (1.0 / frq) * 2
-  with_fx :reverb, mix: 0.5, room: 0.7 do
-    with_fx :echo, amp: 0.25, mix: 0.5, phase: del, decay: 2 do
-      #with_fx :ring_mod, freq: frq do
-      with_fx :slicer, mix: 0.5, phase: [0.25, 0.5, 0.75].choose do
+  4.times do
+    frq = midi_to_hz(notes.tick)
+    del = (1.0 / frq) * 2
+    with_fx :reverb, mix: 0.5, room: 0.7 do
+      with_fx :echo, amp: 0.25, mix: 0.5, phase: del, decay: 2 do
+        #with_fx :ring_mod, freq: frq do
+        with_fx :slicer, mix: 0.5, phase: [0.25, 0.5, 0.75].choose do
 
-        2.times do
-          sample :ambi_choir, pan: 0.5, amp: 0.18, rate: (ring -1, 0.5, -2, -0.5, 1).tick(:ambi)
+          2.times do
+            sample :ambi_choir, pan: 0.5, amp: 0.1, rate: (ring -0.5, 1, 0.5).tick(:ambi)
+            sleep 6
+          end
           sleep 6
         end
-        sleep 6
       end
+      #end
     end
-    #end
   end
 end
 
 live_loop :lunar_sweep do
 
   #sync :none
-  tick_set 3
-  frq = midi_to_hz(notes.tick)
-  del = (1.0 / frq) * 2
   with_fx :reverb, mix: 0.5, room: 0.7 do
     with_fx :bitcrusher, bits: 12 do
       with_fx :slicer, mix: 0.75, phase: (ring 0.5, 0.25, 0.75, 1).tick(:ambi) do
@@ -76,16 +82,18 @@ live_loop :lunar_sweep do
 end
 
 live_loop :sine_bass do
-
+  #sync :none
   use_synth :sine
   #  n = (knit :c2, 3, :ds2, 1)
   n = (knit :c2, 4, :ds2, 1, :f2, 1)
-  play n.tick, amp: 0.25, decay: 0.75, sustain_level: 0.5, sustain: 0.75, release: 1#, cutoff: 80
-  sleep 4
+  4.times do
+    play n.tick, amp: 0.25, decay: 0.75, sustain_level: 0.5, sustain: 0.75, release: 1#, cutoff: 80
+    sleep 4
+  end
 end
 
 live_loop :runner do
-
+  #sync :none
   use_synth :blade
   nts = chord(:c1, :minor, num_octaves: 3).shuffle
   4.times do
@@ -98,8 +106,8 @@ end
 
 live_loop :kick do
 
-  4.times do
-    sample :bd_pure, rate: 0.85, amp: 0.94
+  32.times do
+    sample :bd_pure, rate: 0.85, amp: 0.9
     #sleep 2
     sleep [1, 0.5, 1.5, 0.5, 0.5].ring.tick
   end
@@ -107,12 +115,18 @@ live_loop :kick do
 end
 
 live_loop :pale_rider do
-  sample :drum_cymbal_soft, rate: 0.8, amp: 0.04
-  sleep (knit 4, 2, 8, 2, 2, 2, 16, 2).tick
+  4.times do
+    sample :drum_cymbal_soft, rate: [0.8, 0.65].choose, amp: 0.04
+    sleep (knit 4, 2, 8, 2, 2, 2, 16, 2).tick
+  end
 end
 
 live_loop :brush do
-  sample :elec_cymbal, rate: 4, amp: 0.075, attack: 0.0275, cutoff: 85, res: 0.1
-  #sleep 0.5
-  sleep (knit 0.5, 2, 0.25, 2, 0.5, 2, 0.25, 1, 0.125, 2, 0.5, 2).ring.tick
+  64.times do
+    sample :elec_cymbal, rate: 4 * rdist(0.008, 1), amp: 0.075, attack: 0.0275, cutoff: 85, res: 0.1
+    #sleep 0.5
+    sleep (knit 0.5, 2, 0.25, 2, 0.5, 2, 0.25, 1, 0.125, 2, 0.5, 2).ring.tick
+  end
 end
+
+##
