c901ce730d7012f88e91d4628f74fb1ee6a728c5
Nanomancer <nanomancer@live.co.uk>
2015-09-11 00:23:49 +0100

    slight sync problem...

--- Codewave_0.1.rb
+++ Codewave_0.1.rb
@@ -10,11 +10,12 @@ use_cue_logging true
 
 live_loop :resonatrix do
 
-  #cue :all
-  #cue :kick
+  #cue :fx
+  #cue :synths
+  cue :drums
 
   notes = chord([:c1, :c2].choose, :minor, num_octaves: 2).shuffle
-  4.times do
+  2.times do
     with_fx :reverb, mix: 0.5, room: 0.7 do
       with_fx :lpf, cutoff: 100 do
         2.times do
@@ -23,13 +24,13 @@ live_loop :resonatrix do
 
             frq = midi_to_hz(notes.tick)
             with_fx :echo, amp: 0.3, mix: 0.85, phase: 1.0 / frq, decay: 2 do
-              sample :ambi_haunted_hum, beat_stretch: 4, pan: -0.5, amp: 0.35, rate: (ring -0.25, 0.5, -1, 0.25, -2).tick(:ambi)
+              sample :ambi_haunted_hum, beat_stretch: 4, pan: -0.6, amp: 0.35, rate: (ring -0.25, 0.5, -1, 0.25, -2).tick(:ambi)
             end
             sleep [16, 8, 4, 16, 2].ring.look(:ambi)
 
             frq = midi_to_hz(notes.tick)
             with_fx :echo, amp: 0.3, mix: 0.85, phase: 1.0 / frq, decay: 2 do
-              sample :ambi_haunted_hum, beat_stretch: 4, pan: -0.5, amp: 0.35, rate: (ring 0.25, -0.5, 1, -0.25, 2).look(:ambi)
+              sample :ambi_haunted_hum, beat_stretch: 4, pan: 0.6, amp: 0.35, rate: (ring 0.25, -0.5, 1, -0.25, 2).look(:ambi)
             end
             sleep [16, 8, 4, 16, 2].ring.look(:ambi)
             #tick
@@ -44,7 +45,7 @@ end
 
 live_loop :choir do
   notes = chord(:c1, :minor, num_octaves: 2).shuffle
-  sync :all
+  #sync :fx
   4.times do
     frq = midi_to_hz(notes.tick)
     del = (1.0 / frq)# * 2
@@ -54,10 +55,10 @@ live_loop :choir do
         #with_fx :slicer, mix: 0.35, phase: [0.25, 0.5, 0.75].choose do
 
         2.times do
-          sample :ambi_choir, pan: 0.5, amp: 0.1, rate: (ring -0.5, -1, 0.5).tick(:ambi)
+          sample :ambi_choir, pan: [1, 0, -1].choose, amp: 0.1, rate: (ring 0.25, -0.33, 0.5, -0.66, -1).tick(:ambi)
           sleep 6
         end
-        sleep 6
+        #sleep 6
         #end
       end
       #end
@@ -67,7 +68,7 @@ end
 
 live_loop :lunar_sweep do
 
-  sync :all
+  sync :fx
   with_fx :reverb, mix: 0.5, room: 0.7 do
     with_fx :bitcrusher, bits: [10, 12, 14].choose do
 
@@ -84,37 +85,10 @@ live_loop :lunar_sweep do
   end
 end
 
-live_loop :sine_bass do
-  #sync :all
-  use_synth :sine
-  #  n = (knit :c2, 3, :ds2, 1)
-  n = (knit :c2, 4, :ds2, 1, :f2, 1)
-  4.times do
-    play n.tick, amp: 0.25, decay: 0.75, sustain_level: 0.5, sustain: 0.75, release: 1#, cutoff: 80
-    sleep 4
-  end
-end
-
-live_loop :runner do
-
-  #sync :all
-  use_synth :blade
-  notes = chord(:c3, :minor, num_octaves: 3).shuffle
-  4.times do
-    with_fx :echo, phase: 2, decay: 4 do
-      play notes.tick, amp: 0.04, attack: 2, sustain: 1, release: 3, cutoff: 85
-      sleep [6, 10, 12, 6, 14, 6].ring.look
-    end
-  end
-end
-
 live_loop :kick do
 
-  sync :kick
-  #cue :all
-  #cue :brush
-  #cue :ride
-  #cue :brush
+  sync :drums
+  #cue :drums
 
   32.times do
     sample :bd_pure, rate: 0.85 * rdist(0.008, 1), amp: 0.9
@@ -126,20 +100,47 @@ end
 
 live_loop :brush do
 
-  sync :brush
+  sync :n
   64.times do
-    sample :elec_cymbal, rate: 4 * rdist(0.008, 1), amp: 0.075, attack: 0.0275, cutoff: 85, res: 0.1
-    #sleep 2
-    sleep (knit 0.5, 2, 0.25, 2, 0.5, 2, 0.25, 1, 0.125, 2, 0.5, 2).ring.tick
+    sample :elec_cymbal, rate: 4 * rdist(0.008, 1), amp: 0.075, attack: 0.0275, cutoff: 85, res: 0.1, pan: -0.25
+    sleep 1
+    #sleep (knit 0.5, 2, 0.25, 2, 0.5, 2, 0.25, 1, 0.125, 2, 0.5, 2).ring.tick
   end
 end
 
 live_loop :pale_rider do
 
-  sync :ride
+  sync :n
+  #sync :drums
+
   4.times do
-    sample :drum_cymbal_soft, rate: [0.8, 0.65].choose * rdist(0.008, 1), amp: 0.04
+    sample :drum_cymbal_soft, rate: [0.8, 0.65].choose * rdist(0.008, 1), amp: 0.04, pan: 0.25
     sleep (knit 4, 2, 8, 2, 2, 2, 16, 2).tick
+  end
+end
+
+live_loop :sine_bass do
+  sync :n
+  use_synth :sine
+  #  n = (knit :c2, 3, :ds2, 1)
+  n = (knit :c2, 4, :ds2, 1, :f2, 1)
+  4.times do
+    play n.tick, amp: 0.15, decay: 0.75, sustain_level: 0.5, sustain: 0.75, release: 1#, cutoff: 80
+    sleep 4
+  end
+  #stop
+end
+
+live_loop :runner do
+
+  sync :n
+  use_synth :blade
+  notes = chord(:c3, :minor, num_octaves: 3).shuffle
+  4.times do
+    with_fx :echo, phase: 2, decay: 4 do
+      play notes.tick, amp: 0.04, attack: 2, sustain: 1, release: 3, cutoff: 85
+      sleep [6, 10, 12, 6, 14, 6].ring.look
+    end
   end
 end
 
