bf75d391251d63d18ca4aa6f9493bb5db8556372
Nanomancer <nanomancer@live.co.uk>
2015-10-02 00:55:31 +0100

    pre-vid test

--- Codewave/Codewave_0.2.rb
+++ Codewave/Codewave_0.2.rb
@@ -16,79 +16,82 @@ end
 
 #######################
 use_bpm 60
-set_volume! 4
+set_volume! 5
 set_sched_ahead_time! 3
-use_cue_logging true
-
+use_cue_logging false
+use_random_seed 100
 ##############  BASS  #########################
 
 live_loop :pulsar do
-  cue :hum
-  cue :drn
-  cue :fx
-  cue :run
+  puts "Pulsar"
+  #cue :res
+  # cue :drn
+  #cue :fx
+  #cue :trans
 
   autosync(:pulse)
   use_synth :growl
-  cut = [70, 75, 80, 85, 90, 85, 80, 75].ring.tick(:cut)
-  notes = (knit :c3, 2, :ds3, 1, :c3, 1)
-  #notes = (knit :c3, 3, :ds3, 1, :c3, 1, :b2, 1)
-
+  cut = [60, 65, 70, 75, 80, 85, 80, 75, 70, 65].ring.tick(:cut)
   #notes = (knit :c3, 4, :ds3, 1, :b2, 1)
+  #notes = (knit :c3, 2, :ds3, 1, :c3, 2, :b2, 1)
+  notes = (knit :c3, 2, :ds3, 1, :c3, 1)
   with_fx :reverb, mix: 0.3, room: 0.3, amp: 1 do
-    notes.size.times do
-      play notes.tick, amp: 0.03, attack: 1, sustain: 1, release: 2, cutoff: cut
+    (notes.size * 4).times do
+      play notes.tick, amp: 0.1, attack: 1, sustain: 1, release: 2, cutoff: cut, res: 0.2
       sleep 8
     end
   end
-  # stop
+  #stop
+  # sleep [8, 16, 24].choose
 end
 
 live_loop :drone do
   #tick reset(:as)
   #autosync(:drn)
-  cue :pulse
+  #cue :pulse
   scl = scale([:c5].choose, :harmonic_minor, num_octaves: 1)
   # scl = chord([:c1, :c2, :c3].choose, :minor, num_octaves: 2)
   notes = mk_rand_scale(scl, 4)
 
-  puts "Dronescale: #{notes}"
+  puts "Drone sequence: #{notes}"
   (notes.size * 2).times do
     frq = midi_to_hz(notes.tick)
     del = (1.0 / frq)# * 2
-    with_fx :echo, amp: 0.4, mix: 1, phase: del, decay: 2 do
-      sample :ambi_drone, attack: 0.6, pan: 0, amp: 0.4, rate: 0.5 # (ring 0.25, 0.5).tick(:ambi)
+    with_fx :echo, amp: 0.5, mix: 1, phase: del, decay: 2 do
+      sample :ambi_drone, attack: 0.6, pan: 0, amp: 0.7, rate: 0.5
       sleep 8
     end
   end
   #stop
 end
 
-live_loop :res_hum do
-
+live_loop :resonance do
   #cue :drn
-  autosync(:hum)
+  autosync(:res)
+  #notes = chord([:c1, :c2, :c3].choose, :minor, num_octaves: 2).shuffle
   notes = scale(:c4, :harmonic_minor, num_octaves: 1).shuffle
-  vol = 0.35
+  puts "Resonance sequence: #{notes}"
+
+  vol = 0.5
 
   notes.size.times do
 
     with_fx :reverb, mix: [0.5, 0.6, 0.7, 0.8].choose, room: [0.6, 0.7, 0.8].choose do
       with_fx :compressor, threshold:  0.4 do
-        with_fx :lpf, res: 0.2, cutoff: [70, 75, 80, 85].choose do
+        with_fx :lpf, res: 0.1, cutoff: [70, 75, 80, 85].choose do
           phase = [0.25, 0.5, 0.75, 1, 1.5, 2].choose
-
+          puts "Resonance AM: #{phase}"
           with_fx :slicer, mix: [1, 0.75, 0.5, 0.25].choose, smooth_up: phase * 0.5, smooth_down: phase * 0.125, phase: phase do
 
             frq = midi_to_hz(notes.tick)
-            with_fx :echo, amp: 0.3, mix: 0.85, phase: 1.0 / frq, decay: 2 do
-              sample :ambi_haunted_hum, beat_stretch: 4, pan: -0.6, amp: vol, rate: (ring -0.25, 0.5, 0.25).tick(:ambi)
+            with_fx :echo, amp: 0.5, mix: 0.85, phase: 1.0 / frq, decay: 2 do
+              sample :ambi_haunted_hum, beat_stretch: 4, pan: -0.75, amp: vol, rate: (ring -0.25, 0.5, 0.25).tick(:ambi)
             end
             sleep [16, 8, 16].ring.look(:ambi)
 
             frq = midi_to_hz(notes.tick)
-            with_fx :echo, amp: 0.3, mix: 0.85, phase: 1.0 / frq, decay: 2 do
-              sample :ambi_haunted_hum, beat_stretch: 4, pan: 0.6, amp: vol, rate: (ring 0.25, -0.5, -0.25).look(:ambi)
+            with_fx :echo, amp: 0.5, mix: 0.85, phase: 1.0 / frq, decay: 2 do
+              sample :ambi_haunted_hum, beat_stretch: 4, pan: 0.75, amp: vol, rate: (ring 0.25, -0.5, -0.25).look(:ambi)
             end
             sleep [16, 8, 16].ring.look(:ambi)
           end
@@ -100,27 +103,27 @@ live_loop :res_hum do
   #stop
 end
 
-live_loop :runner do
+live_loop :transmission do
 
-  autosync(:run)
+  autosync(:trans)
   use_synth :blade
   chd = chord(:c1, :minor, num_octaves: 2).shuffle
   scl = scale([:c4, :c5, :c6].choose, :harmonic_minor, num_octaves: 1)
 
   2.times do
     notes = mk_rand_scale(scl, 3)
-    puts "Runner scale: #{notes}"
-    slp = [[3,3,2], [6,6,4], [8,8,4]].choose.ring
-    puts "sleepsize: #{slp.size}"
+    puts "Transmission sequence: #{notes}"
+    slp = [[3,3,2], [6,6,4], [8,8,4], [12,6,12]].choose.ring
 
     (slp.size * 2).times do
       att, sus, rel = slp.tick * 0.3, slp.look * 0.2, slp.look * 0.5
-      phs = [0.25, 0.5, 0.75, 1].choose
+      phase = [0.25, 0.5, 0.75, 1].choose
+      mod_frq = rdist(0.0125, 0.5) * midi_to_hz(chd.tick(:chd))
+      puts "Transmission AM: #{phase} | Ring mod frq: #{mod_frq}"
       with_fx :echo, mix: 0.25, phase: 1.5, decay: 4 do
-        with_fx :ring_mod, freq: rdist(0.0125, 0.5) * midi_to_hz(chd.tick(:chd)) do
-          with_fx :slicer, mix: [0.9, 0.5, 0.25, 0.125].choose, smooth_up: phs * 0.5, smooth_down: phs * 0.125, phase: phs do
-            play notes.look, amp: 0.0125, attack: att, sustain: sus, release: rel, cutoff: 85
-            puts "Runner sleep: #{slp.look}"
+        with_fx :ring_mod, freq: mod_frq do
+          with_fx :slicer, mix: [0.9, 0.5, 0.25, 0.125].choose, smooth_up: phase * 0.5, smooth_down: phase * 0.125, phase: phase do
+            play notes.look, amp: 0.025, attack: att, sustain: sus, release: rel, cutoff: 85
             sleep slp.look
           end
         end
@@ -131,17 +134,18 @@ live_loop :runner do
   sleep [4, 8, 12, 16, 32].choose
 end
 
-live_loop :lunar_sweep do
-
+live_loop :static do
+  puts "Static"
   autosync(:fx)
-  with_fx :reverb, mix: 0.5, room: 0.4 do
+  with_fx :reverb, mix: 0.5, room: 0.5 do
     with_fx :bitcrusher, bits: [12, 14].choose, sample_rate: [4000, 8000, 12000].choose do
-
+      phase = [0.5, 0.25, 0.75, 1].ring
       2.times do
-        with_fx :slicer, smooth_up: 0.125, mix: 0.75, phase: (ring 0.5, 0.25, 0.75, 1).tick do
-          sample :ambi_lunar_land, cutoff: 110, beat_stretch: 8, amp: 0.05, rate: (ring -1, -2, -0.5).look
+        with_fx :slicer, smooth_up: 0.125, mix: 0.75, phase: phase.tick do
+          puts "Static AM: #{phase.look}"
+          sample :ambi_lunar_land, cutoff: 110, beat_stretch: 8, amp: 0.125, rate: (ring -1, -2, -0.5).look
           sleep [8, 4, 16].ring.look
-          sample :ambi_lunar_land, cutoff: 110, beat_stretch: 8, amp: 0.05, rate: (ring 1, 2, 0.5).look
+          sample :ambi_lunar_land, cutoff: 110, beat_stretch: 8, amp: 0.125, rate: (ring 1, 2, 0.5).look
           sleep 16
         end
         sleep 24
