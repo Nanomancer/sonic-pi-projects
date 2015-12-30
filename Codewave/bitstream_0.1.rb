8974ef1a0eb972fa8677c33328cab9cf61ad4f32
Nanomancer <nanomancer@live.co.uk>
2015-12-30 02:48:50 +0000

    four scale options

--- Codewave/bitstream_0.1.rb
+++ Codewave/bitstream_0.1.rb
@@ -20,40 +20,12 @@ use_random_seed Time.now.usec
 
 #   end
 
-live_loop :origin_untraceable do
-  use_synth :dark_ambience
-
-  scl = scale([:c5, :c6].choose, :harmonic_minor, num_octaves: 2)
-  notes = mk_rand_scale(scl, 8)
-  sprd1 = [2,3,5,6,7,8].choose
-  rtm_arr = (spread sprd1, 8)
-  2.times do
-
-    4.times do
-      tick_reset(:rtm)
-      tick_reset
-      sprd1.times do
-        if rtm_arr.tick(:rtm)
-          play note notes.tick
-        end
-        sleep 0.25#[0.125, 0.25, 0.5].choose
-      end
-    end
-    # if one_in 3
-    #   sleep [4,8].choose
-    # end
-  end
-  # if one_in 2
-  #   sleep [8,16].choose
-  # end
-end
-
 live_loop :fourfour do
-  if one_in 5
+  if one_in 6
     sleep [8, 16].choose
   else
     8.times do
-      sample :bd_haus, amp: 1, cutoff: 75, rate: 0.9
+      sample :bd_haus, amp: 0.4, cutoff: 75, rate: 0.9
       sleep 0.5
     end
   end
@@ -64,10 +36,17 @@ live_loop :looper do
     sleep [8, 16].choose
   else
     4.times do
-      sample :loop_breakbeat, beat_stretch: 2, amp: 0.5
+      sample :loop_breakbeat, beat_stretch: 2, amp: 0.3
       sleep 2
     end
   end
+end
+
+live_loop :looper2 do
+  if not one_in 5
+    sample :loop_mika, beat_stretch: 16, amp: 0.2
+  end
+  sleep 16
 end
 
 live_loop :offbass do
@@ -81,26 +60,11 @@ live_loop :offbass do
       tick
       16.times do
         sleep 0.25
-        play notes.look, release: 0.25, amp: 0.7, cutoff: 85, res: 0.1
+        play notes.look, release: 0.25, amp: 0.45, cutoff: 85, res: 0.1
         sleep 0.25
       end
     end
   end
-end
-
-live_loop :crystal_entity do
-  notes = (ring 60, 62, 63, 65, 68, 71, 72).shuffle
-
-  vol = 0.8
-
-  frq = midi_to_hz(notes.tick)
-  # with_fx :ring_mod, freq: frq do
-  if not one_in 5
-    sample :loop_mika, beat_stretch: 16, amp: 0.4
-  end
-  sleep 16
-  # end
-  # end
 end
 
 
@@ -121,7 +85,7 @@ live_loop :bitstream do
   if one_in 4
     sleep [16,32].choose
   else
-    with_fx :bitcrusher, mix: 0.4, bits: [5, 6].choose, sample_rate: (range 500, 3000, step: 250, inclusive: true).choose do
+    with_fx :bitcrusher, mix: 0.4, bits: [5, 6].choose, sample_rate: (range 1000, 3000, step: 250, inclusive: true).choose do
       with_fx :slicer, phase: 0.125, smooth_up: 0.01, smooth_down: 0.01 do
 
         with_fx :reverb, mix: 0.3, room: 0.4, amp: 1 do
@@ -130,7 +94,7 @@ live_loop :bitstream do
             cut = (range 50, 110, step: 10, inclusive: true)
             cut2 = (range 110, 50, step: 10, inclusive: true)
 
-            s = play notes.tick, note_slide: 0.5, amp: 0.25, attack: 0.1, sustain: 8, release: 0.1, cutoff: cut.look(:cut), cutoff_slide: 4, res: 0.2
+            s = play notes.tick, note_slide: 0.5, amp: 0.15, attack: 0.1, sustain: 8, release: 0.1, cutoff: cut.look(:cut), cutoff_slide: 4, res: 0.2
             control s, cutoff: cut2.look(:cut)
             sleep 4
             control s, cutoff: cut.tick(:cut)
@@ -141,5 +105,42 @@ live_loop :bitstream do
         end
       end
     end
+  end
+end
+
+
+
+scales_arr = []
+4.times do
+  scl = scale([:c5, :c6].choose, :harmonic_minor, num_octaves: 2)
+  scales_arr << mk_rand_scale(scl, 8)
+end
+puts scales_arr
+
+live_loop :crystal_entity do
+  use_synth :dark_ambience
+
+
+  notes = scales_arr.choose
+
+  sprd1 = [2,3,4,5,6,7,8].choose
+  rtm_arr = (spread sprd1, 8)
+  2.times do
+    4.times do
+      tick_reset(:rtm)
+      tick_reset
+      8.times do
+        if rtm_arr.tick(:rtm)
+          play note notes.look(:rtm), amp: 1
+        end
+        sleep 0.25#[0.125, 0.25, 0.5].choose
+      end
+    end
+    if one_in 3
+      sleep [4,8].choose
+    end
+  end
+  if one_in 2
+    sleep [4,8].choose
   end
 end
