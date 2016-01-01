775e77773326f3a83b79dc5424c9117ed562d7d1
Nanomancer <nanomancer@live.co.uk>
2016-01-01 20:26:26 +0000

    tidied up mix

--- Codewave/bitstream_0.1.rb
+++ Codewave/bitstream_0.1.rb
@@ -1,7 +1,8 @@
 set_volume! 5
 epoch_seed = Time.now.usec
 puts epoch_seed
-use_random_seed epoch_seed #247444
+use_random_seed epoch_seed #247444#
+# use_random_seed 704169
 set_sched_ahead_time! 1
 # a = 0
 # fadeout = false
@@ -34,36 +35,55 @@ live_loop :fourfour do
 end
 
 live_loop :looper do
-  if one_in 5
+  if one_in 6
     sleep [8, 16].choose
   else
-    4.times do
-      sample :loop_breakbeat, beat_stretch: 2, amp: 0.3
-      sleep 2
+    len = [1,2].choose
+    if one_in 4
+      slp = len * 2
+    else
+      slp = len
+    end
+    8.times do
+      sample :loop_industrial, beat_stretch: len, amp: 0.125
+      sleep slp
     end
   end
 end
 
 live_loop :looper2 do
-  if not one_in 5
-    sample :loop_mika, beat_stretch: 16, amp: 0.2
+  if one_in 6
+    sleep [8, 16].choose
+  else
+    len = [8,16].choose
+    if len > 8 && (one_in 4)
+      slp = len * 2
+    else
+      slp = len
+    end
+    2.times do
+      sample :loop_garzul, beat_stretch: len, amp: 0.15
+      sleep slp
+    end
   end
-  sleep 16
 end
 
 live_loop :offbass do
 
   notes = [:c2, :ds2, :c2].ring
   use_synth :subpulse
-  if one_in 5
+  if one_in 8
     sleep [16, 32].choose
   else
-    notes.size.times do
-      tick
-      16.times do
-        sleep 0.25
-        play notes.look, release: 0.4, amp: 0.45, cutoff: 70, res: 0.1
-        sleep 0.25
+    with_fx :distortion, amp: 0.1, distort: 0.97 do
+      notes.size.times do
+        tick
+        16.times do
+          sleep 0.25
+          play notes.look, release: 0.4, amp: 0.2, cutoff: 75, res: 0.1
+          synth :sine, note: notes.look, amp: 0.1, attack: 0.04, release: 0.4
+          sleep 0.25
+        end
       end
     end
   end
@@ -80,23 +100,26 @@ live_loop :bitstream do
 
   autosync(:pulse)
   autostop(rrand max_t*0.65, max_t) # (rrand_i 5, 8)
-  puts "Bitstream"
 
   # notes = (knit :c2, 2, :ds2, 1, :c2, 1)
   notes = (chord :c2, :minor)
+
   if one_in 4
     sleep [16,32].choose
   else
-    with_fx :bitcrusher, mix: 0.4, bits: [5, 6].choose, sample_rate: (range 1000, 3000, step: 250, inclusive: true).choose do
-      with_fx :slicer, phase: 0.125, smooth_up: 0.01, smooth_down: 0.01 do
+    puts "Bitstream"
+    with_fx :slicer, phase: [0.25, 0.125].choose, smooth_up: 0.01, smooth_down: 0.01 do
+      with_fx :bitcrusher, mix: 0.4, bits: [4, 5, 6].choose, sample_rate: (range 1500, 3000, step: 250, inclusive: true).choose do
 
-        with_fx :reverb, mix: 0.3, room: 0.4, amp: 1 do
-          notes.size.times do
+        with_fx :reverb, mix: 0.3, room: 0.4, amp: 0.5 do
+          # notes.size.times do
+          4.times do
+
             # cut = [55, 60, 65, 70, 75, 80, 85, 80, 75, 70, 65, 60].ring
             cut = (range 50, 110, step: 10, inclusive: true)
             cut2 = (range 110, 50, step: 10, inclusive: true)
 
-            s = play notes.tick, note_slide: 0.5, amp: 0.15, attack: 0.1, sustain: 8, release: 0.1, cutoff: cut.look(:cut), cutoff_slide: 4, res: 0.2
+            s = play notes.tick, note_slide: 0.5, amp: 0.1, attack: 0.1, sustain: 8, release: 0.1, cutoff: cut.look(:cut), cutoff_slide: 4, res: 0.2
             control s, cutoff: cut2.look(:cut)
             sleep 4
             control s, cutoff: cut.tick(:cut)
