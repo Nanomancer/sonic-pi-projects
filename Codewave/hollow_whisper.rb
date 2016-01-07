38969c41852e90513c64d27d49af86490e4e54b7
Nanomancer <nanomancer@live.co.uk>
2016-01-07 15:36:45 +0000

    quick tidy

--- Codewave/hollow_whisper.rb
+++ Codewave/hollow_whisper.rb
@@ -1,5 +1,8 @@
+## Hollow Whispers - Coded by Nanomancer
+
 use_random_seed Time.now.usec
 
+############ DEFINE FUNCTIONS #################
 
 define :ambi_seq do |chord_arr, vol, len=8|
 
@@ -14,10 +17,11 @@ define :ambi_seq do |chord_arr, vol, len=8|
     release: len*rrand(0.3, 0.6), cutoff: rrand(75,110), pan: 0.5
 end
 
-####################################################
 
+#############  PAD  ###############
 
 live_loop :ambipad do
+
   use_synth :hollow
   12.times do
     len = [16, 8, 8].ring.look
@@ -30,14 +34,52 @@ live_loop :ambipad do
       sleep len
     end
   end
-  if one_in 3
-    sleep [8,16,32].choose
-  end
+  if one_in 3 then sleep [8,16,32].choose end
 end
+
+
+############ GENERATE WHISPER SEQUENCE  ##############
+
+scales_arr = []
+2.times do
+  scl = scale([:a3, :a4].choose, :hungarian_minor, num_octaves: 2)
+  scales_arr << mk_rand_scale(scl, 4)
+end
+puts scales_arr
+
+
+############### WHISPER LEAD ##################
+
+live_loop :whisper do
+
+  use_synth :dark_ambience
+  # autosync(:trans)
+  # autostop(rrand max_t*0.8, max_t) # (rrand_i 5, 7)
+
+  notes = scales_arr.choose
+  tick_reset
+  slp = [[3,3,4,6], [2,2,4], [4,4,8], [4,4]].choose.ring
+
+  (slp.size * 2).times do
+    att, sus, rel = slp.tick * 0.1, slp.look * 0.3, slp.look * 0.6
+    phase = [0.25, 0.5, 0.75, 1].choose
+    with_fx :echo, mix: 0.25, phase: 1.5, decay: 4 do
+      with_fx :slicer, mix: rrand(0.2, 0.5), smooth_up: phase * 0.5, smooth_down: phase * 0.125, phase: phase do
+        play notes.look, amp: 0.4, attack: att, sustain: sus, release: rel, cutoff: 85 # unless one_in(3)
+        sleep slp.look
+      end
+    end
+  end
+  if one_in(3) then sleep [8, 16, 32].choose end
+end
+
 
 ####################################################
 
+################  DARKHARP HIT ################
+
 live_loop :darkharp do
+
   sync :d_harp
   use_synth :zawa
 
@@ -56,55 +98,17 @@ live_loop :darkharp do
   end
 end
 
-####################################################
 
-scales_arr = []
-2.times do
-  scl = scale([:a3, :a4].choose, :hungarian_minor, num_octaves: 2)
-  scales_arr << mk_rand_scale(scl, 4)
-end
-puts scales_arr
-
-
-
-live_loop :whisper do
-  use_synth :dark_ambience
-
-  # autosync(:trans)
-  # autostop(rrand max_t*0.8, max_t) # (rrand_i 5, 7)
-
-  notes = scales_arr.choose
-  tick_reset
-  slp = [[3,3,4,6], [2,2,4], [4,4,8], [4,4]].choose.ring
-  # slp = [[3,3,2], [4,4,2], [2,3,3], [3,2,2]].choose.ring
-
-  (slp.size * 2).times do
-    att, sus, rel = slp.tick * 0.1, slp.look * 0.3, slp.look * 0.6
-    phase = [0.25, 0.5, 0.75, 1].choose
-    with_fx :echo, mix: 0.25, phase: 1.5, decay: 4 do
-      with_fx :slicer, mix: rrand(0.2, 0.5), smooth_up: phase * 0.5, smooth_down: phase * 0.125, phase: phase do
-        play notes.look, amp: 0.4, attack: att, sustain: sus, release: rel, cutoff: 85 # unless one_in(3)
-        sleep slp.look
-      end
-    end
-  end
-  if one_in(3) then sleep [8, 16, 32].choose end
-end
-
-####################################################
-
-
-
-####################################################
-
+############### BASS THROB ################
 
 live_loop :throb do
-  use_synth :prophet
 
+  use_synth :prophet
   if one_in 5 then multi = 2
   elsif one_in 2 then multi = 0.5
   else multi = 1 end
   rst = true if one_in(4)
+
   12.times do
     notes = (degree [[:i, :viii].ring.look, :i, :vii, :i, :iii, :vii].ring.tick, :A1, :hungarian_minor)
     slp = [4,2,2].ring.look
@@ -119,14 +123,15 @@ live_loop :throb do
   end
 end
 
-####################################################
 
+###############  DRUMS 1  ################
 
 live_loop :doombeat do
-  if one_in(2) then sleep [8, 16, 32].choose end
+  if one_in(2) then sleep [16, 32, 64].choose end
   16.times do
     sample :loop_industrial, beat_stretch: 2, amp: 0.225, cutoff: 75
     sleep 2
   end
 end
+
 ###
