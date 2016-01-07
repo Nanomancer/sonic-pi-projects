b93ae9e5b30735db15f14ab050376c41010b5385
Nanomancer <nanomancer@live.co.uk>
2016-01-07 03:39:47 +0000

    darkharp arrangement

--- Codewave/hollow_whisper.rb
+++ Codewave/hollow_whisper.rb
@@ -1,26 +1,17 @@
-# use_synth :hollow
-
-# live_loop :ambidark do |idx|
-#   # test = (chord_degree idx+1, :A3, :hungarian_minor, 3)
-#   test = (chord_degree [:i, :iii, :vi, :vii].choose, :A3, :hungarian_minor, 3)
-
-#   puts test
-#   play test, attack: 3,release: 3
-#   sleep 4
-# end
+use_random_seed Time.now.usec
 
 
 define :ambi_seq do |chord_arr, vol, len=8|
 
   play chord_arr[0], amp: vol*rrand(0.8, 1.2),
     attack: len*rrand(0.5, 0.8), sustain: len*rrand(0.25, 0.5),
-    release: len*rrand(0.5, 0.8), cutoff: rrand(75,130), pan: -0.5
+    release: len*rrand(0.5, 0.8), cutoff: rrand(75,110), pan: -0.5
   play chord_arr[1], amp: vol*rrand(0.8, 1.2),
     attack: len*rrand(0.4, 0.7), sustain: len*rrand(0.25, 0.5),
-    release: len*rrand(0.4, 0.7), cutoff: rrand(75,130)
+    release: len*rrand(0.4, 0.7), cutoff: rrand(75,110)
   play chord_arr[2], amp: vol*rrand(0.8, 1.2),
     attack: len*rrand(0.3, 0.6), sustain: len*rrand(0.25, 0.5),
-    release: len*rrand(0.3, 0.6), cutoff: rrand(75,130), pan: 0.5
+    release: len*rrand(0.3, 0.6), cutoff: rrand(75,110), pan: 0.5
 end
 
 ####################################################
@@ -28,17 +19,22 @@ end
 
 live_loop :ambipad do
   use_synth :hollow
-  len = 8#[16, 8, 8, 16, 8, 8].ring.look
-  # chords1 = (chord_degree [:i, :vi, :iii, :vii].ring.tick, :A2, :hungarian_minor, 3)
-  # chords2 = (chord_degree [:i, :vi, :iii, :vii].ring.look, :A3, :hungarian_minor, 3)
-  # chords1 = (chord_degree [[:i, :viii].ring.look, :i, :vii, :i, :iii, :vii].ring.tick, :A2, :hungarian_minor, 3)
-  chords2 = (chord_invert (chord_degree [:i, :i, :vii, :i, :iii, :vii].ring.tick,
-                           :A3, :hungarian_minor, 3), rrand_i(0,3))
+  12.times do
+    len = [16, 8, 8, 16, 8, 8].ring.look
+    # chords1 = (chord_degree [:i, :vi, :iii, :vii].ring.tick, :A2, :hungarian_minor, 3)
+    # chords2 = (chord_degree [:i, :vi, :iii, :vii].ring.look, :A3, :hungarian_minor, 3)
+    # chords1 = (chord_degree [[:i, :viii].ring.look, :i, :vii, :i, :iii, :vii].ring.tick, :A2, :hungarian_minor, 3)
+    chords2 = (chord_invert (chord_degree [:i, :i, :vii, :i, :iii, :vii].ring.tick,
+                             :A2, :hungarian_minor, 3), rrand_i(0,3))
 
-  with_fx :reverb, mix: 0.7 do
-    # ambi_seq(chords2, 0.8, len)
-    # ambi_seq(chords2, len)
-    sleep len
+    with_fx :reverb, mix: 0.6 do
+      ambi_seq(chords2, 0.35, len)
+      # ambi_seq(chords2, len)
+      sleep len
+    end
+  end
+  if one_in 3
+    sleep [8,16,32].choose
   end
 end
 
@@ -53,7 +49,7 @@ live_loop :darkharp do
              :A3, :hungarian_minor, 2)
 
   with_fx :echo, mix: 0.4, phase: 1.5, decay: 10 do
-    with_fx :reverb, mix: 0.8, room: 0.6, amp: 0.35 do
+    with_fx :reverb, mix: 0.8, room: 0.6, amp: 0.3 do
       chords2.size.times do
         play chords2.tick(:chrd)+[12, -12].choose, amp: 0.09, attack: 0.0625, release: 1.25, cutoff: 97
         sleep 0.25#1.0/3
@@ -92,7 +88,7 @@ live_loop :whisper do
     puts "Whisper Amp Mod: #{phase}}"
     with_fx :echo, mix: 0.25, phase: 1.5, decay: 4 do
       with_fx :slicer, mix: rrand(0.2, 0.5), smooth_up: phase * 0.5, smooth_down: phase * 0.125, phase: phase do
-        play notes.look, amp: 0.4, attack: att, sustain: sus, release: rel, cutoff: 85
+        play notes.look, amp: 0.5, attack: att, sustain: sus, release: rel, cutoff: 85
         sleep slp.look
       end
     end
@@ -137,8 +133,12 @@ end
 
 
 live_loop :doombeat do
-  sample :loop_industrial, beat_stretch: 2, amp: 0.2, cutoff: 75
-  sleep 2
+  16.times do
+    sample :loop_industrial, beat_stretch: 2, amp: 0.25, cutoff: 75
+    sleep 2
+  end
+  if one_in 3
+    sleep [8, 16, 32].choose
+  end
 end
-
 ###
