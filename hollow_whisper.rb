6a0e0710738ad0fa7704c6eebb7e1e3b0d4f5ce5
Nanomancer@live.co.uk <nanomancer@live.co.uk>
2016-01-09 15:53:01 +0000

    bass sends tonic to darkharp - test version

--- hollow_whisper.rb
+++ hollow_whisper.rb
@@ -3,9 +3,9 @@
 set_volume! 5
 rseed = Time.now.usec
 use_random_seed rseed
-File.open('/home/james/Copy/sonic_pi/whisper_seeds.txt', 'a+') do |f|
-  f.puts("Seed: #{rseed}")
-end
+# File.open('/home/user/Copy/sonic_pi/whisper_seeds.txt', 'a+') do |f|
+#   f.puts("Seed: #{rseed}")
+# end
 
 puts "Seed: #{rseed}"
 
@@ -119,19 +119,24 @@ end
 
 live_loop :darkharp, auto_cue: false do
 
-  sync :d_harp
+  map = sync :d_harp
   use_synth :zawa
 
-  chords2 = (chord_degree [:i, :i, :vii, :i, :iii, :vii].ring.tick,
-             :A3, :hungarian_minor, 2)
+  chords2 = (chord_degree map[:degree], :A3, :hungarian_minor, 5)
+  puts "Darkharp hit degree: #{map[:degree]}"
 
-  with_fx :echo, mix: 0.4, phase: 1.5, decay: 10 do
+  with_fx :echo, mix: 0.4, phase: [1.5, 0.75].choose, decay: 10 do
     with_fx :reverb, mix: 0.8, room: 0.6, amp: 0.3 do
-      chords2.size.times do
-        play chords2.tick(:chrd)+[12, -12].choose, amp: 0.09, attack: 0.06, release: 1.25,
+      # chords2.size.times do
+      2.times do
+        note1 = [0, 1].choose
+        note2 = note1 + [2,3].choose
+        oct = [12, -12].choose
+        puts "Darkharp notes- 1: #{note1} oct: #{oct}| 2: #{note2}"
+        play chords2[note1]+oct, amp: 0.09, attack: 0.06, release: 1.25,
           cutoff: rdist(3, 95)
         sleep 0.25
-        play chords2.tick(:chrd), amp: 0.09, attack: 0.06, release: 1.5,
+        play chords2[note2], amp: 0.09, attack: 0.06, release: 1.5,
           cutoff: rdist(3, 95)
         sleep 0.25
       end
@@ -149,19 +154,24 @@ live_loop :throb do
   elsif one_in 2 then multi = 0.5
   else multi = 1 end
   # multi = 0.5
-  rst, rst_harp = one_in(4), one_in(6)
+  rst, rst_harp = one_in(4), one_in(600000)
 
   cue :a_pad, multi: multi
 
   12.times do
-    notes = (degree [[:i, :viii].ring.look, :i, :vii, :i, :iii, :vii].ring.tick, :A1, :hungarian_minor)
+    # notes = (degree [[:i, :viii].ring.look, :i, :vii, :i, :iii, :vii].ring.tick, :A1, :hungarian_minor)
+    deg = [[:i, :viii].ring.look, :i, :vii, :i, :iii, :vii].ring.tick
+    puts "Bass degree: #{deg}"
+    notes = (degree deg, :A1, :hungarian_minor)
+
+
     slp = [4,2,2].ring.look
     slp = slp*multi
     with_fx :slicer, phase: 0.5, mix: 0.5, smooth_up: 0.125 do
       play notes, amp: 0.13, release: slp+2, cutoff: 80 unless rst == true
       sleep slp*0.5
-      if multi == 2 && one_in(3) && rst_harp == false then cue :d_harp
-      elsif multi == 1 && one_in(8) && rst_harp == false then cue :d_harp end
+      if multi == 2 && one_in(1) && rst_harp == false then cue :d_harp, degree: deg# 3
+      elsif multi == 1 && one_in(1) && rst_harp == false then cue :d_harp, degree: deg end # 8
       sleep slp*0.5
     end
   end
