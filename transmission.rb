2ba8644fe48ceb8af6a9ef702ab27e8c91ce865c
Nanomancer <nanomancer@live.co.uk>
2016-03-29 19:25:25 +0100

    video / soundcloud version_2

--- transmission.rb
+++ transmission.rb
@@ -1,4 +1,3 @@
-
 ## Transmission - Origin Unknown -Tuned Resonators in C minor
 ## Random Seed Version: 746742
 ## Coded by Nanomancer
@@ -22,11 +21,17 @@ SEED = Time.now.usec
 # use_random_seed SEED # 746742 # 100
 use_random_seed 746742 # 100
 
+sleep 2
+sample :elec_blip
+puts "SYNC"
+sleep 8
+
 
 ################ FUNCTIONS ########################
 
 define :stopwatch do |int, max, fade|
-  ## interval in seconds(just for log timer), max in mins, fade in secs
+  ## interval in seconds (display for log timer),
+  ## max in mins, fade in secs
   count = 0
   set_mixer_control! amp: 1, amp_slide: 0.1
   with_bpm 60 do
@@ -67,7 +72,7 @@ end
 #############  CLOCK  #####################
 
 in_thread do
-  stopwatch(15, max_t, max_t*60*0.1)
+  stopwatch(30, max_t, max_t*60*0.1)
 end
 
 
@@ -76,14 +81,15 @@ end
 live_loop :pulsar, sync: :pulse, auto_cue: false do
 
   use_synth :growl
-  autostop(rrand max_t*0.75, max_t) # (rrand_i 5, 8)
+  autostop(rrand max_t*0.75, max_t)
   puts "Pulsar"
   notes = (knit :c3, 2, :ds3, 1, :c3, 1)
 
   with_fx :reverb, mix: 0.3, room: 0.3, amp: 1 do
     notes.size.times do
       cut = range(55, 85, step: 5, inclusive: true).mirror.ring.tick(:cut)
-      play notes.tick, amp: 0.25, attack: 1.125, sustain: 1.25, release: 3, cutoff: cut, res: 0.2
+      play notes.tick, amp: 0.25, attack: 1.125,
+        sustain: 1.25, release: 3, cutoff: cut, res: 0.2
       sleep 8
     end
   end
@@ -95,23 +101,21 @@ end
 
 live_loop :drone, auto_cue: false do
 
-  autostop(max_t) #(rrand_i 6, 8)
+  autostop(max_t)
   autocue(:prb, (rrand 0, max_t*0.3))
   autocue(:pulse, (rrand 0, max_t*0.4))
   autocue(:stc, (rrand 0, max_t*0.3))
   autocue(:trans, (rrand max_t*0.2, max_t*0.5))
-
   scl = scale(:c5, :harmonic_minor, num_octaves: 1)[0..4]
-
   notes = mk_rand_scale(scl, 4)
 
   puts "Drone sequence: #{notes}"
-
   notes.size.times do
     frq = midi_to_hz(notes.tick)
-    del = (1.0 / frq)# * 2
+    del = 1.0 / frq
     with_fx :echo, amp: 1, mix: 1, phase: del, decay: 2 do
-      sample :ambi_drone, attack: 0.6, pan: 0, amp: 1, rate: 0.5, cutoff: 117.5
+      sample :ambi_drone, attack: 0.6,
+        pan: 0, amp: 1, rate: 0.5, cutoff: 117.5
       sleep 8
     end
   end
@@ -123,33 +127,35 @@ end
 live_loop :probe, sync: :prb, auto_cue: false do
 
   autostop(rrand max_t*0.7, max_t)
+  notes = (ring 63, 65, 68, 71, 72).shuffle
+  vol = 1
 
-  notes = (ring 60, 62, 63, 65, 68, 71, 72).shuffle
   puts "Probe sequence: #{notes}"
-  vol = 0.925
 
   4.times do
 
     phase = [0.25, 0.5, 0.75, 1, 1.5, 2].choose
-    puts "Probe AM: #{phase}"
-    with_fx :lpf, res: 0.1, cutoff: [70, 75, 80, 85].choose do
+    with_fx :lpf, res: 0.1, cutoff: rrand(70, 85) do
       with_fx :compressor, threshold:  0.4 do
-        with_fx :slicer, mix: [1, 0.75, 0.5, 0.25].choose, smooth_up: phase * 0.5, smooth_down: phase * 0.125, phase: phase do
-          with_fx :reverb, mix: [0.5, 0.6, 0.7, 0.8].choose, room: [0.6, 0.7, 0.8].choose do
+        with_fx :slicer, mix: rrand(0.25, 0.75),
+        smooth_up: phase * 0.5, smooth_down: phase * 0.125, phase: phase do
+          with_fx :reverb, mix: rrand(0.6, 0.8), room: [0.6, 0.7, 0.8].choose do
 
-            frq = midi_to_hz(notes.tick)
-            with_fx :echo, amp: 0.7, mix: 0.85, phase: 1.0 / frq, decay: 2 do
-              sample :ambi_haunted_hum, beat_stretch: 4, pan: -0.75, amp: vol, rate: (ring -0.25, 0.5, 0.25).tick(:ambi)
+            2.times do
+              frq = midi_to_hz(notes.tick)
+              puts "Probe freq: #{frq.round(2)} | Mod: #{phase}"
+              with_fx :echo, amp: 0.8, mix: 0.85, phase: 1.0 / frq, decay: 2.5 do
+                sample :ambi_haunted_hum,
+                  beat_stretch: 4,
+                  pan: [0.75, -0.75].ring.look,
+                  amp: vol,
+                  rate: (ring 0.25, -0.25).tick(:ambi)
+              end
+              sleep 16
             end
-            sleep [16, 8, 16].ring.look(:ambi)
 
-            frq = midi_to_hz(notes.tick)
-            with_fx :echo, amp: 0.7, mix: 0.85, phase: 1.0 / frq, decay: 2 do
-              sample :ambi_haunted_hum, beat_stretch: 4, pan: 0.75, amp: vol, rate: (ring 0.25, -0.5, -0.25).look(:ambi)
-            end
-            sleep [16, 8, 16].ring.look(:ambi)
           end
-          sleep [4, 6, 8, 12, 16, 24].choose
+          sleep [12, 16, 24, 32].choose
         end
       end
     end
@@ -164,7 +170,7 @@ scales_arr = []
   scl = scale([:c5, :c6].choose, :harmonic_minor, num_octaves: 2)
   scales_arr << mk_rand_scale(scl, 3)
 end
-puts scales_arr
+# puts scales_arr
 
 live_loop :transmission, sync: :trans, auto_cue: false do
 
@@ -173,18 +179,21 @@ live_loop :transmission, sync: :trans, auto_cue: false do
   chd = chord(:c1, :minor, num_octaves: 2).shuffle
   notes = scales_arr.choose
 
-  puts "Transmission sequence: #{notes}"
   slp = [[3,3,2], [4,4,2], [6,6,3], [8,8,4]].choose.ring
+  puts "Transmission pattern: #{slp}"
 
   (slp.size * 2).times do
     att, sus, rel = slp.tick * 0.3, slp.look * 0.2, slp.look * 0.5
     phase = [0.25, 0.5, 0.75, 1].choose
     mod_frq = rdist(0.0125, 0.5) * midi_to_hz(chd.tick(:chd))
-    puts "Transmission AM: #{phase} | Ring mod frq: #{mod_frq}"
+    # puts "Transmission - T: #{slp.look} | RM frq: #{mod_frq.round(2)}"
+
     with_fx :echo, mix: 0.25, phase: 1.5, decay: 4 do
       with_fx :ring_mod, freq: mod_frq do
-        with_fx :slicer, mix: [0.9, 0.5, 0.25, 0.125].choose, smooth_up: phase * 0.5, smooth_down: phase * 0.125, phase: phase do
-          play notes.look, amp: 0.1, attack: att, sustain: sus, release: rel, cutoff: 85
+        with_fx :slicer, mix: rrand(0.125, 0.75),
+        smooth_up: phase * 0.5, smooth_down: phase * 0.125, phase: phase do
+
+          play notes.look, amp: 0.09, attack: att, sustain: sus, release: rel, cutoff: 85
           sleep slp.look
         end
       end
@@ -202,15 +211,20 @@ live_loop :static, sync: :stc, auto_cue: false do
   mod_frq = midi_to_hz(scl)
   phs = [0.125, 0.5, 0.25, 0.75, 1].choose
   len = [8, 10, 12, 16, 24, 32].choose
+
   puts "Static - AM: #{phs} | Len: #{len}"
 
   with_fx :hpf, cutoff: 60 do
     with_fx :ring_mod, freq: mod_frq do
-      with_fx :slicer, smooth_up: phs*0.25, mix: rrand(0.3,0.9), phase: phs do
+      with_fx :slicer, smooth_up: phs*0.25,
+      mix: rrand(0.3,0.9), phase: phs do
         with_fx :reverb, mix: 0.5, room: 0.6 do
+
           cut = rrand(60, 120)
-          amt = rrand(60, 120) # cut + (rrand 0, 40)
-          s = play :c4, amp: 0.0375, attack: len*0.5, release: len*0.5, cutoff: cut, cutoff_slide: len*0.5, res: (rrand 0.01, 0.6)
+          amt = rrand(60, 120)
+          s = play :c4, amp: 0.0375, attack: len*0.5,
+            release: len*0.5, cutoff: cut,
+            cutoff_slide: len*0.5, res: (rrand 0.01, 0.6)
           control s, cutoff: amt
           sleep len*0.5
           control s, cutoff: cut
