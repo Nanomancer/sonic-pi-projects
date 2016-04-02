e1ad928521176f49d7b646ff3cad865b07098df3
Nanomancer <nanomancer@live.co.uk>
2016-04-02 02:30:56 +0100

    Whisper renamed epoch seed

--- Epoch_Seed.rb
+++ Epoch_Seed.rb
@@ -1,20 +1,9 @@
-## Hollow Whisper - Coded by Nanomancer
-
-######## to do: #########
-
-# drums / perc - add subtle cymbal, reverse ride into forward? create loop using generators?
-# key change start with bass
-# sustained zawa for faster bass section?
-# whisper melodic generation - non random solution required
-# Tidy chances etc - put harp chance in harp!?
-# Set up global clock/arrangement gen
-
-#########################
+## Working title - Epoch Seed - Coded by Nanomancer
 
 set_volume! 5
 rseed = Time.now.usec
 use_random_seed rseed
-File.open('Copy/sonic_pi/whisper_seeds.txt', 'a+') do |f|
+File.open('Sync/sonic_pi/whisper_seeds.txt', 'a+') do |f|
   f.puts("Seed: #{rseed}")
 end
 # 236249
@@ -48,15 +37,15 @@ define :mk_rand_scale do |scale, len = 8|
 end
 
 
-define :bass_patt do |trans, rst, no_rest, rst_harp, deg, multi, slp|
-  play scale(:A1, :hungarian_minor)[deg-1], amp: 0.13, attack: rdist(0.01, 0.02), release: slp.tick(:slp)*multi*1.25*rdist(0.1, 1), cutoff: rdist(2.1, 80) unless rst
+define :bass_patt do |rst, no_rest, rst_harp, deg, multi, slp|
+  play (degree deg, :A1, :hungarian_minor), amp: 0.13, attack: rdist(0.01, 0.02), release: slp.tick(:slp)*multi*1.25*rdist(0.1, 1), cutoff: rdist(2.1, 80) unless rst
   puts "Bass degree: #{deg}"
   sleep slp.look(:slp) *0.5*multi
   unless no_rest && multi == 2
-    if multi == 2 && one_in(2) && rst_harp == false then cue :d_harp, transpose: trans, degree: deg, multi: multi# 3
-    elsif multi == 1 && one_in(3) && rst_harp == false then cue :d_harp, transpose: trans, degree: deg
+    if multi == 2 && one_in(2) && rst_harp == false then cue :d_harp, degree: deg, multi: multi# 3
+    elsif multi == 1 && one_in(3) && rst_harp == false then cue :d_harp, degree: deg
     end
-  else cue :d_harp, transpose: trans, degree: deg
+  else cue :d_harp, degree: deg
   end
   sleep slp.look(:slp) *0.5*multi
 end
@@ -71,12 +60,12 @@ live_loop :ambipad do
 
   3.times do
     len = [8, 8, 16].ring.tick
-    chords2 = (chord_invert (chord_degree [1, [7, 5].choose, 1].ring.look,
+    chords2 = (chord_invert (chord_degree [:i, [:vii, :v].choose, :i].ring.look,
                              :A2, :hungarian_minor, 3), rrand_i(0,3))
     # puts "Ambipad: #{chords2} | length: #{len}"
 
     with_fx :reverb, mix: 0.6 do
-      # varichord(chords2, 0.35, len)
+      varichord(chords2, 0.35, len)
       sleep len
     end
   end
@@ -131,48 +120,37 @@ live_loop :darkharp, auto_cue: false do
 
   map = sync :d_harp
   use_synth :zawa
-  if map[:degree] < 9
-    deg = map[:degree]
-  else
-    deg = map[:degree] % 8
-  end
-  chords2 = (chord_degree deg, :A3, :hungarian_minor, 5)
+
+  chords2 = (chord_degree map[:degree], :A3, :hungarian_minor, 5)
   puts "Darkharp degree: #{map[:degree]}"
 
-  if map[:multi] == 2 then slp = [0.5,0.75].choose
-  else slp = [0.25, 0.5].choose
+  if map[:multi] == 2 then slp = [0.25,0.5,0.75].choose
+  else slp = 0.25
   end
   # slp = 0.75
   note1 = [0, 1].choose
   if one_in(3) then reps = 1
   else reps = 2
   end
-  trans = map[:transpose]
-  if (map[:degree] -trans == 1 || map[:degree] -trans == 8) && note1 == 0 then note2 = note1 + [1,2,4].choose
-  elsif (map[:degree]-trans == 1 || map[:degree] -trans == 8) && note1 == 1 then note2 = note1 + [1,4].choose
-  elsif map[:degree] -trans == 3 && note1 == 0 then note2 = note1 + [1,3].choose
-  elsif map[:degree] -trans == 3 && note1 == 1 then note2 = note1 + [1,2,4].choose
 
-  elsif map[:degree] -trans == 7 && note1 == 0 then note2 = note1 + [1,4].choose
-  elsif map[:degree] -trans == 7 && note1 == 1 then note2 = note1 + [3,4].choose
+  if map[:degree] == :i || map[:degree] == :viii && note1 == 0 then note2 = note1 + [1,2,3,4].choose
+  elsif map[:degree] == :i || map[:degree] == :viii && note1 == 1 then note2 = note1 + [1,3,4].choose
+  elsif map[:degree] == :iii && note1 == 0 then note2 = note1 + [1,3].choose
+  elsif map[:degree] == :iii && note1 == 1 then note2 = note1 + [1,2,4].choose
+
+  elsif map[:degree] == :vii && note1 == 0 then note2 = note1 + [1,4].choose
+  elsif map[:degree] == :vii && note1 == 1 then note2 = note1 + [3,4].choose
   end
-
-  if slp == 0.75 then del = dice(2)
-  else del = 1.5
-  end
-
-  with_fx :echo, mix: 0.4, phase: del, decay: 10 do
+  with_fx :echo, mix: 0.4, phase: 1.5, decay: 10 do
     with_fx :reverb, mix: 0.7, room: 0.6, amp: 0.5 do
       reps.times do
 
         oct = [12, -12].choose
-        puts "Darkharp notes- N1= #{note1} - N2= #{note2}"
+        puts "Darkharp notes- N1= #{note1+1} - N2= #{note2+1}"
         play chords2[note1]+oct, amp: 0.1, attack: rdist(0.01, 0.06), release: rdist(0.125, 1.25), cutoff: rdist(3, 95), pan: 1
         sleep slp
         play chords2[note2], amp: 0.1, attack: rdist(0.01, 0.07), release: rdist(0.1, 1.5), cutoff: rdist(3, 95), pan: -1
         sleep slp
-        # note1 += 1
-        # note2 += 1
       end
     end
   end
@@ -194,43 +172,28 @@ live_loop :throb do
 
   cue :a_pad, multi: multi
   deg1_reps = [3,9].choose
-  trans = 6# [0, 4].choose
+
   with_fx :slicer, phase: 0.5, mix: 0.5, smooth_up: 0.125 do
-    # 2.times do
-    #   deg1_reps.times do
-
-    #     deg1 = [[1, 8].ring.tick(:oct), 1, 7].ring.tick
-    #     bass_patt(rst, no_rest, rst_harp, deg1, multi, slp)
-    #   end
-
-    #   3.times do
-    #     deg2 = [1, 3, 7].ring.tick
-    #     bass_patt(rst, no_rest, rst_harp, deg2, multi, slp)
-    #   end
-    # end
-
     2.times do
       deg1_reps.times do
 
-        deg1 = [[1, 8].ring.tick(:oct), 1, 7].ring.tick+trans
-        bass_patt(trans, rst, no_rest, rst_harp, deg1, multi, slp)
+        deg1 = [[:i, :viii].ring.tick(:oct), :i, :vii].ring.tick
+        bass_patt(rst, no_rest, rst_harp, deg1, multi, slp)
       end
-      unless trans > 0
-        3.times do
-          deg2 = [1, 3, 7].ring.tick+trans
-          bass_patt(trans, rst, no_rest, rst_harp, deg2, multi, slp)
-        end
+
+      3.times do
+        deg2 = [:i, :iii, :vii].ring.tick
+        bass_patt(rst, no_rest, rst_harp, deg2, multi, slp)
       end
     end
   end
 end
-
 ###############  DRUMS 1  ################
 
 live_loop :doombeat do
   if one_in(3) then sleep [16, 32, 64].choose end
   16.times do
-    sample :loop_industrial, beat_stretch: 2, amp: 0.225, cutoff: 72
+    sample :loop_industrial, beat_stretch: 2, amp: 0.225, cutoff: range(60, 85, step: 2.5).mirror.ring.tick
     sleep 2
   end
 end
---
+++ epoch_notes.md
@@ -0,0 +1,15 @@
+Hollow whisper
+
+    rename!
+        Epoch Seed?
+    video ideas?
+        will come from title
+        keywords
+            dark
+            foreboding
+            ominous
+            crystal
+            slowmarch
+            doom
+            whisper
+    code
