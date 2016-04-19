183f8578c5b56374120ec05d769067fefccca6d1
Nanomancer <nanomancer@live.co.uk>
2016-04-19 12:05:13 +0100

    Dark eastern - vid version

--- eastern_ambient.rb
+++ eastern_ambient.rb
@@ -1,6 +1,11 @@
 ## Eastern pluck & 'safari' percussion
 ## Coded by Nanomancer
 
+##| set_volume! 5
+##| puts "SYNC"
+##| sample :elec_blip, amp: 0.5
+##| sleep 8
+
 set_volume! 0.55
 # set_mixer_control! amp: 0.1, amp_slide: 0.1
 set_mixer_control! amp: 0.1, amp_slide: 0.1
@@ -8,14 +13,10 @@ sleep 1
 set_mixer_control! amp: 5, amp_slide: 12
 use_bpm 120
 
-puts "SYNC"
-sample :elec_blip, amp: 0.5
-sleep 8
-
 live_loop :safari, delay: 32 do
   with_fx :reverb, mix: 0.4, room: 0.6 do
     if one_in(3) then sample :loop_safari, amp: 0.9, finish: 0.5, rate: 0.5, beat_stretch: 16
-    elsif one_in(3) then sample :loop_safari, amp: 0.9, start: 0.5, rate: 0.5, beat_stretch: 16 end
+    elsif one_in(2) then sample :loop_safari, amp: 0.9, start: 0.5, rate: 0.5, beat_stretch: 16 end
     
     if one_in(3) then sample :loop_safari, amp: 0.35, rate: 1, beat_stretch: 16 end
     sleep 16
@@ -32,16 +33,18 @@ seeds = [323699.5012207031, 323705.5012207031, 323723.5012207031,
          323729.5012207031, 323735.5012207031, 323747.5012207031,
          323759.5012207031, 323771.5012207031, 323831.5012207031,
          323837.5012207031, 323861.5012207031, 323867.5012207031,
-         323897.5012207031, 323993.5012207031, 324017.5012207031, #15 onwards # 18No worrie total (from 0)
-         323729.5012207031, 323755.5012207031, 323677.5012207031, 323761.5012207031].ring ## last line from old list
+         323897.5012207031, 323993.5012207031, 324017.5012207031,
+         323729.5012207031, 323755.5012207031, 323677.5012207031,
+         323761.5012207031].ring
 
 ##| autoseeder = []
+vol = 0.3
 
 live_loop :eastern_twang do |idx|
   use_synth :pluck
+  
   with_random_seed seeds[idx] do
     puts "SEED: #{current_random_seed} loop no.: #{idx}"
-    if idx == 22 then stop end
     ##| autoseeder << current_random_seed
     notes = scale(:a3, :harmonic_minor, num_octaves: dice(2)).pick(4)
     multi = [0.5,1,1.5,2].choose
@@ -54,8 +57,10 @@ live_loop :eastern_twang do |idx|
     with_fx :echo, mix: 0.4, phase: 2, max_phase: 4 do
       with_fx :reverb, mix: 0.65, room: 0.85 do
         reps.times do
-          play notes.tick, amp: 0.3*rdist(0.09, 1), cutoff: rdist(15, 115), pitch: rdist(0.1, 0)
+          play notes.tick, amp: vol*rdist(0.09, 1),
+            cutoff: rdist(15, 115), pitch: rdist(0.1, 0)
           sleep slp.look*multi
+          if idx >= 22 && vol >= 0.0125 then vol -= 0.0125 end
         end
       end
     end
