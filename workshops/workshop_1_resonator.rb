4e87263e03899d01fbd0d0015aae9b430f181222
Nanomancer <nanomancer@live.co.uk>
2020-04-24 05:55:48 +0100

    renamed variables

--- workshops/workshop_1_resonator.rb
+++ workshops/workshop_1_resonator.rb
@@ -3,11 +3,11 @@ live_loop :resonator do
   notes = chord(:c2, :minor, num_octaves: 2).shuffle
 
   frq = midi_to_hz(notes.tick)
-  #delay = 1 / frq
-  delay = 1.0 / frq
-  puts delay
+  #phaseRate = 1 / frq
+  phaseRate = 1.0 / frq
+  puts phaseRate
   
-  with_fx :echo, amp: 0.5, mix: 0.85, phase: delay, decay: 2 do
+  with_fx :echo, amp: 0.5, mix: 0.85, phase: phaseRate, decay: 2 do
     sample :bd_haus, amp: 0.5
     sleep 0.5
   end
