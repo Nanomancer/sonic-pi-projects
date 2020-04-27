21d48b306f4682b6d590d976f4b9e878013adb09
Nanomancer <nanomancer@live.co.uk>
2020-04-27 02:17:24 +0100

    ready to jam

--- hirajoshiAndEuclid.rb
+++ hirajoshiAndEuclid.rb
@@ -3,41 +3,52 @@
 
 use_bpm 120
 
-live_loop :euclideanSequencer do
+live_loop :EBM, sync: :clock do
+  pattern_1, pattern_2, pattern_3 = spread( 3, 8 ), spread( 2, 7 ), spread( 3, 10 )
   use_synth :prophet
-  #################################
-  pattern_1 = get[:pattern_3_8]
-  pattern_2 = get[:pattern_2_7]
-  pattern_3 = get[:pattern_3_10]
-  #################################
-
-  ##| notes = ( chord :c2,'m7+9', num_octaves: 2 )
-  notes = (scale :c2, :hirajoshi, num_octaves: 2)
-  root = 0
-  noteTriggerPattern = get[:pattern_15_16]
-
-  ##| set :dynamics, [1, 0.75, 0.9, 0.6, 0.44].ring
-  set :dynamicPattern, [1, 0.75, 0.9, 0.6].ring
-
-  tick_reset
-  16.times do
-    currentDegree = root
-    clock = tick(step: 1)
-
-    ##| clock = tick(step: 2)
-    ##| clock = tick(step: -1)
-    ##| clock = tick(step: -3)
-    ##| clock = tick(step: 3)
-    if pattern_1[clock + 2] then currentDegree = 7 end
-    if pattern_[clock + 2] then currentDegree -= 1 end
-    if pattern_3[clock + 4] then currentDegree = 5 end
-    ##| if pattern_1[clock + 4] then currentDegree -= 2 end
-
-    if noteTriggerPattern[clock] then play notes[currentDegree],
-                                           amp: get[:dynamics][clock]
- ##| cutoff: rand_i(65..75)
-          end
-
-    sleep 0.25
+  ##| 2.times do
+  (get[:editQuantisation]).times do
+    tick_reset
+    #################################
+    ##| notes = ( chord :c2,'m7+9', num_octaves: 2 )
+    notes = (scale :c2, :hirajoshi, num_octaves: 2)
+    root = 0
+    ##| noteTriggerPattern = spread(8, 12)
+    ##| noteTriggerPattern = spread(9, 12)
+    ##| noteTriggerPattern = spread(10, 12)
+    ##| noteTriggerPattern = spread(11, 12)
+    ##| noteTriggerPattern = spread(9, 16)
+    ##| noteTriggerPattern = spread(11, 16)
+    ##| noteTriggerPattern = spread(13, 16)
+    noteTriggerPattern = spread(15, 16)
+    ##| dynamicPattern = [1, 0.75, 0.9, 0.6, 0.44].ring
+    dynamicPattern = [1, 0.75, 0.9, 0.6].ring
+    16.times do
+      currentDegree = root
+      
+      clock = tick(step: 1) ##| clock steps - # 1 / -1 / 2 / 3 / -3
+      
+      if pattern_1[clock + 2] then currentDegree = 7 end
+      if pattern_2[clock + 2] then currentDegree -= 1 end
+      if pattern_3[clock + 4] then currentDegree = 5 end
+      ##| if pattern_1[clock + 4] then currentDegree -= 2 end
+      
+      if noteTriggerPattern[clock] then play notes[currentDegree],
+          amp: dynamicPattern[clock],
+          ##| cutoff: 92
+          cutoff: [100, 90, 105, 88, 110].ring.look * 0.99
+      end
+      sleep 0.25
+    end
   end
+  
+end
+
+live_loop :clock, cue: id do | idx |
+  ##| puts cue("/live_loop/clock")
+  puts idx
+  4.times do
+    sleep 1
+  end
+  idx + 1
 end
